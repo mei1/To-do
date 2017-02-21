8e81d582b5d902f03c0817cd35204fede74b68c6
qimei <qimei@192.168.0.10>
2017-02-21 11:41:58 +0800

    added pojo

---
+++ src/org/qimei/training/pojo/NewTask.java
@@ -0,0 +1,24 @@
+package org.qimei.training.pojo;
+
+public class NewTask {
+
+	String taskTitle;
+	String taskContent;
+
+	public String getTaskTitle() {
+		return taskTitle;
+	}
+
+	public void setTaskTitle(String taskTitle) {
+		this.taskTitle = taskTitle;
+	}
+
+	public String getTaskContent() {
+		return taskContent;
+	}
+
+	public void setTaskContent(String taskContent) {
+		this.taskContent = taskContent;
+	}
+
+}
---
+++ src/org/qimei/training/pojo/TaskGroup.java
@@ -0,0 +1,24 @@
+package org.qimei.training.pojo;
+
+public class TaskGroup {
+
+	String groupName;
+	NewTask newTask;
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	public NewTask getNewTask() {
+		return newTask;
+	}
+
+	public void setNewTask(NewTask newTask) {
+		this.newTask = newTask;
+	}
+
+}
