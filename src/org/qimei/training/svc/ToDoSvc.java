bfb34a655c6f1f0c7aa5cec972e14ee188ebabd0
qimei <qimei@192.168.0.10>
2017-02-22 16:01:05 +0800

    added insert and get services to the app

---
+++ src/org/qimei/training/svc/ToDoSvc.java
@@ -0,0 +1,42 @@
+package org.qimei.training.svc;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.qimei.training.db.ToDoDBHelper;
+import org.qimei.training.pojo.Task;
+import org.qimei.training.pojo.TaskGroup;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class ToDoSvc {
+	
+	private static final Logger logger = Logger.getLogger(ToDoSvc.class);
+
+	@RequestMapping(value = "inserttaskdata", method = RequestMethod.POST, produces = "application/json")
+	public int insertIntoTask(@RequestBody Task taskObj) throws SQLException {
+		logger.info("Insert into task request");
+		return ToDoDBHelper.getInstance().insertIntoNewTask(taskObj);
+	}
+	
+	@RequestMapping(value = "inserttaskgroupdata", method = RequestMethod.POST, produces = "application/json")
+	public int insertIntoTaskGroup(@RequestBody TaskGroup taskGroupObj) throws SQLException{
+		logger.info("Insert into task group request");
+		return ToDoDBHelper.getInstance().insertIntoTaskGroup(taskGroupObj);
+	}
+	
+	@RequestMapping(value = "alltasks", method = RequestMethod.GET, produces = "application/json")
+	public List<Task> getAllTask() throws SQLException{
+		logger.info("Get all task request");
+		return ToDoDBHelper.getInstance().getAllTasksFromDB();
+	}
+	
+	@RequestMapping(value = "alltaskgroups", method = RequestMethod.GET, produces = "application/json")
+	public List<TaskGroup> getAllTaskGroup() throws SQLException{
+		logger.info("Get all task group request");
+		return ToDoDBHelper.getInstance().getAllTaskGroupFromDB();
+	}
+
+}
